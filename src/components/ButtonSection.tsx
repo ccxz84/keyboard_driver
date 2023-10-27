f081b0889fafaeaa85dd3d1975b44f5385be63f0
ccxz84 <ccxz84@naver.com>
2023-10-27 17:44:17 +0900

    초기 버전

---
+++ src/components/ButtonSection.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ButtonComponent from './ButtonComponent';
+// import '../style/ButtonSection.scss';
+const { ipcRenderer } = window.require('electron');
+
+const ButtonSection: React.FC = () => {
+
+  const startRecording = () => {
+    ipcRenderer.send('start-recording');
+  };
+
+  const endRecording = () => {
+    ipcRenderer.send('end-recording');
+  };
+
+  return (
+    <div className="button-section">
+      <ButtonComponent text={'녹화 시작'} onClick={startRecording} />
+      <ButtonComponent text={'녹화 종료'} onClick={endRecording } />
+    </div>
+  );
+}
+
+export default ButtonSection;
