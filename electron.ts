327c2968fcb53329c2dee1fefeba70014ddcefa4
ccxz84 <ccxz84@naver.com>
2023-12-28 03:55:02 +0900

    [chore] 오류 수정

--- electron.ts
+++ electron.ts
@@ -38,32 +38,6 @@ app.on('activate', () => {
     if (mainWindow === null) createWindow();
 });
 
-// ipcMain.on('start-recording', () => {
-//     const allDevices = devices();
-//     const targetDevice = allDevices.filter(device => device.vendorId === 0x1d6b && device.productId === 0x0104);
-//     console.log(targetDevice);
-    
-//     if (targetDevice[1] && targetDevice[1].path) {
-//         const device = new HID(targetDevice[1].path);
-//         device.write([0x00, 0x00, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]); // HID 장치로 데이터 전송
-//     } else {
-//         console.error("장치 또는 경로를 찾을 수 없습니다.");
-//     }
-// });
-
-// ipcMain.on('end-recording', () => {
-//     const allDevices = devices();
-//     const targetDevice = allDevices.filter(device => device.vendorId === 0x1d6b && device.productId === 0x0104);
-//     console.log(targetDevice);
-    
-//     if (targetDevice[1] && targetDevice[1].path) {
-//         const device = new HID(targetDevice[1].path);
-//         device.write([0x00, 0x00, 0x20, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]); // HID 장치로 데이터 전송
-//     } else {
-//         console.error("장치 또는 경로를 찾을 수 없습니다.");
-//     }
-// });
-
 ipcMain.on('start-recording', () => {
     const request = new StartRequest();
     MacroGrpcClient.startRequest(request);
